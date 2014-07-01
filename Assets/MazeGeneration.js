87cd8b382990810d68213980c12be990d9b02af4
Luke Olney <lukeolney@gmail.com>
2014-07-01 16:32:53 -0500

    Added ground / maze

--- Assets/MazeGeneration.js
+++ Assets/MazeGeneration.js
@@ -2,7 +2,11 @@
 
 import System.Collections.Generic;
 
-private var gridSize = 50;
+private var gridSize = 5;
+
+public var Player : Transform;
+public var blockPrefabs : GameObject[];
+private var blockTypes = new Dictionary.<String, Texture2D>();
 
 function Start () {
 	var grid = new int[gridSize,gridSize];
@@ -13,6 +17,29 @@ function Start () {
 	
 	var maze = Prim (grid);
 	
+	createSprites(maze);
+	
+}
+
+function createSprites(maze : List.<Vector2>[,]) {
+
+	var pos = Player.position;
+	for (var i = 0; i < gridSize; i++)
+		for (var j = 0; j < gridSize; j++){
+			print("For " + i + j);
+			for(var k = 0; k < maze[i,j].Count; k++) {
+	            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+	            //cube.AddComponent(Rigidbody2D);
+	            if((i - maze[i,j][k].x) == 0)
+	            	cube.transform.localScale = Vector3(1,4,1);
+            	else
+	            	cube.transform.localScale = Vector3(4,1,1);
+	            
+	            cube.transform.position = pos + Vector3 (maze[i,j][k].x * 5, maze[i,j][k].y * 5, 0);
+	            print(maze[i,j][k].x + " " + maze[i,j][k].y);
+     		}
+     }
+     
 }
 
 function Prim (grid : int[,]) : List.<Vector2>[,] {
@@ -30,9 +57,10 @@ function Prim (grid : int[,]) : List.<Vector2>[,] {
 	
 	
 	
-	while(!minHeap.isEmpty()) { // heap is not empty)		
+	while(!minHeap.isEmpty()) { // heap is not empty)	
 		// Take from heap, add to tree
 		do {
+			if(minHeap.isEmpty()) return VNew;
 			var edge = minHeap.remove();
 		}
 		while(VNew[edge.b.x, edge.b.y] != null); // vertex not yet in list
@@ -49,12 +77,14 @@ function Prim (grid : int[,]) : List.<Vector2>[,] {
 }
 
 function addAdjacents(minHeap : BHeap, grid : int[,], i : int, j : int) {
+
 	var adjs :  List.<Vector2> = adjacent(i, j);
 	for(var ii=0; ii<adjs.Count; ii++) {
 		var a : int = adjs[ii].x;
 		var b : int = adjs[ii].y;
 		minHeap.insert(AdjacencyObject(Vector2(i,j), adjs[ii], grid[a, b]));
 	}
+	
 }
 
 function adjacent (i:int, j:int) :  List.<Vector2> {
@@ -77,7 +107,7 @@ function addAdjacent() {
 private class BHeap {
 
 	private var A : AdjacencyObject[];
-	private var size : int;
+	public var size : int;
 	private var maxSize = 500;
 	private var infinity = AdjacencyObject(Vector2(0,0), Vector2(0,0), 9999);
 	
@@ -91,12 +121,20 @@ private class BHeap {
 	}
 	
 	function isEmpty() {
-		if(size == 0) return true;
+		if(size <= 0) return true;
 		else return false;
 	}
 	
 	function insert(elem : AdjacencyObject) {
-		if(size == maxSize) return false;
+		if(size == maxSize) {
+			maxSize *= 2;
+			var B = new AdjacencyObject[maxSize];
+			A.CopyTo(B, 0);
+			A = B;
+			
+			for(var i=size; i<maxSize; i++)
+				A[i] = infinity;
+		}
 		
 		A[size] = elem;
 		upHeapify(size);		
@@ -108,10 +146,11 @@ private class BHeap {
 	}
 	
 	function remove() {
+		if(size == 0) return null;
 		var result = A[0];
 		
 		size--;
-		A[0] = A[size];		
+		A[0] = A[size];
 		A[size] = infinity;	
 		
 		minHeapify(0);
@@ -139,7 +178,7 @@ private class BHeap {
 		var left = (i+1) * 2 - 1;
 		var right = left + 1;
 		
-		if(left > maxSize || right > maxSize)
+		if(left >= maxSize || right >= maxSize)
 			return 0;
 		
 		var smallest = i;
