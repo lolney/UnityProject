4a7dd8b88981c5d0250fdd2c102b51519a922a71
Luke Olney <lukeolney@gmail.com>
2014-07-01 03:26:43 -0500

    Improved movement/collision detection

--- Assets/SimpleMovement.js
+++ Assets/SimpleMovement.js
@@ -1,9 +1,40 @@
 ﻿#pragma strict
 
+public var Speed = 4.0;	
+public var JumpPower = 10.0;	
+
+private var facingRight = true;
+
 function Start () {
 
 }
 
 function Update () {
 
+	var move = Input.GetAxis("Horizontal");
+	
+	// If upside down, stop
+	if(!(transform.eulerAngles.z < 90 || transform.eulerAngles.z > 270))
+		return 0;
+		
+	// Flip
+	if((move > 0 && facingRight) || (move < 0 && !facingRight)) 
+			flip();
+				
+	// Move
+	rigidbody2D.velocity = Vector2(Speed * move, rigidbody2D.velocity.y);
+	
+	// Jump
+	// TODO: replace with collision below check
+	if(Input.GetKey (KeyCode.Space) && rigidbody2D.velocity.y < .01 && rigidbody2D.velocity.y > -.01)
+		rigidbody2D.velocity += Vector2(0, JumpPower);
+		
 }
+
+function flip() {
+	if(facingRight) facingRight = false;
+	else facingRight = true;
+	
+	transform.localScale.x *= -1;
+}
+
