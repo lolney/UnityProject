59a3453351b52e4fcdb750d99eabf42c5d6a18fd
Luke Olney <lukeolney@gmail.com>
2014-07-02 21:17:32 -0500

    New Songs

--- Assets/AudioPlayRandom.js
+++ Assets/AudioPlayRandom.js
@@ -1,10 +1,10 @@
 ﻿#pragma strict
 
-public var src : AudioSource[] = new AudioSource[10];
-public var frequency : int = 1000;
+public var src : AudioSource[];
+public var period : int = 1000;
 
 function Update () {
-	var rand : int = Random.Range(0, frequency);
+	var rand : int = Random.Range(0, period);
 	for(var i=0; i < src.Length; i++)
 		if(rand == i) src[i].Play();
 }
