73265e3ef1651e6c96de348dcc5a505cb807d4bd
Luke Olney <lukeolney@gmail.com>
2014-07-02 19:18:01 -0500

    Ground fixed

--- Assets/ThirdPerson.js
+++ Assets/ThirdPerson.js
@@ -5,6 +5,6 @@ public var Player : Transform;
 function Update () {
 	
 	transform.position.x = Player.position.x;
-	transform.position.y = Player.position.y + Camera.current.orthographicSize / 2;
+	transform.position.y = Player.position.y + 2 * Camera.current.orthographicSize / 3;
 	
 }
