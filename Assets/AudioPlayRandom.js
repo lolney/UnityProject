22c0edf48528835234e3d79d4d285b374b800e01
Luke Olney <lukeolney@gmail.com>
2014-07-02 20:58:57 -0500

    Wee Wee songs

---
+++ Assets/AudioPlayRandom.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+public var src : AudioSource[] = new AudioSource[10];
+public var frequency : int = 1000;
+
+function Update () {
+	var rand : int = Random.Range(0, frequency);
+	for(var i=0; i < src.Length; i++)
+		if(rand == i) src[i].Play();
+}
