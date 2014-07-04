522b5c0b32fb4e472ee95f1906c8f320eaf0a3d5
Luke Olney <lukeolney@gmail.com>
2014-07-03 23:32:56 -0500

    Grass texture

--- Assets/ThirdPerson.js
+++ Assets/ThirdPerson.js
@@ -4,7 +4,10 @@ public var Player : Transform;
 
 function Update () {
 	
+	var offset = ( 2 * ( Camera.main.orthographicSize / 3) - Player.position.y);
+	if(offset < 0) offset = 0;
+		
 	transform.position.x = Player.position.x;
-	transform.position.y = Player.position.y + 2 * Camera.main.orthographicSize / 3;
+	transform.position.y = Player.position.y + offset;
 	
 }
